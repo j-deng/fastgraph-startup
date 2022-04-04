84aad5e9e288358ba44cb44d647299637f1d490d
j-deng <wh-dj@hotmail.com>
2022-04-04 20:47:29 +0800

    Add front-end validation error zhCN messages

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,6 +3,7 @@ import { DefaultApolloClient } from '@vue/apollo-composable'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import fg from 'fastgraph-vue'
+import zhCN from 'joi-messages-zh_cn'
 import App from './App.vue'
 import router from './routes'
 import { apolloClient } from './apollo'
@@ -21,5 +22,9 @@ app.use(Antd)
 app.use(router)
 app.use(i18n)
 app.use(store, key)
-app.use(fg)
+app.use(fg, {
+  joiMessages: {
+    ...zhCN
+  }
+})
 app.mount('#app')
